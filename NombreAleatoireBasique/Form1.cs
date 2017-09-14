e08404f482d6123d3873e17152a195fbc1b9f4a0
TysorHorseRiding <nesquiik3@sfr.fr>
2017-09-14 19:14:26 +0200

    Ajouts principaux

--- NombreAleatoireBasique/Form1.cs
+++ NombreAleatoireBasique/Form1.cs
@@ -12,9 +12,22 @@ namespace NombreAleatoireBasique
 {
     public partial class Form1 : Form
     {
+        Random nombrealeatoire = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int result))
+            {
+                if (int.TryParse(textBox2.Text, out int result2))
+                {
+                    label3.Text = nombrealeatoire.Next(result, result2).ToString();
+                }
+            }
         }
     }
 }
